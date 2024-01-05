5d995a1417ac5fe7f46c7e2462c286fa858868b2
pranjal20498 <60666427+pranjal20498@users.noreply.github.com>
2024-01-05 17:07:09 +0530

    Add files via upload

---
+++ fir2.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 16.04.2023 20:54:47
+// Design Name: 
+// Module Name: fir2
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module fir2(
+
+    );
+endmodule
